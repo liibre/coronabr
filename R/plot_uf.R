75698cd717ed7ab12ec5f29cbbd5c6c1c61818a6
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-05-26 13:46:27 -0300

    change and clean up plot_uf

--- R/plot_uf.R
+++ R/plot_uf.R
@@ -49,67 +49,37 @@ plot_uf <- function(df,
   } else {
     df <- df
   }
+
   state_plot <- df %>%
     dplyr::filter(.data$state %in% states & !is.na(.data$state)) %>%
-    dplyr::mutate(state = reorder(.data$state, -.data$confirmed)) %>%
-    ggplot(aes(x = .data$date, y = .data$confirmed, colour = .data$state)) +
+    ggplot(aes(x = date, y = .data$confirmed, colour = .data$state)) +
     geom_line() +
     geom_point() +
-    labs(x = "Data",
-         y = paste0("N", "\u00fa", "meros de casos confirmados"),
-         title = paste0("Estados com mais de ", n, " casos"),
+    labs(title = paste0("Estados com mais de ", n, " casos"),
          fill = "UF",
          caption = legenda) +
     guides(color = guide_legend("UF")) +
-    scale_x_date(date_breaks = "1 day",
+    scale_x_date(date_breaks = "10 days",
                  date_labels = "%d/%m") +
     scale_color_viridis_d() +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 90),
           legend.title = element_text(size = 7),
           legend.text = element_text(size = 7))
+
   if (tipo == "casos") {
-    state_plot <- state_plot
-
-    # anim_plot <- state_plot %+%
-    #   gganimate::transition_reveal(.data$date) +
-    #   geom_segment(aes(xend = max(.data$date), yend = .data$confirmed), linetype = 2) +
-    #   geom_text(aes(x = max(.data$date), label = .data$state), hjust = 0) +
-    #   coord_cartesian(clip = "off")
-    #
-    # file_name <- "anim_plot_uf_casos"
-
+    state_plot <- state_plot %+%
+      aes(x = .data$date, y = .data$deaths, colour = reorder(.data$state, -.data$confirmed)) +
+      labs(y = paste0("N", "\u00fa", "meros de casos confirmados"),
+           x = "Data")
   }
   if (tipo == "mortes") {
     state_plot <- state_plot %+%
-      aes(x = date, y = .data$deaths, colour = reorder(.data$state, -.data$deaths)) +
+      aes(x = .data$date, y = .data$deaths, colour = reorder(.data$state, -.data$deaths)) +
       labs(y = paste0("N", "\u00fa", "meros de mortes confirmadas"),
            x = "Data")
-    # # só gera o plot
-    # anim_plot <- state_plot %+%
-    #   gganimate::transition_reveal(.data$date) +
-    #   geom_segment(aes(xend = max(.data$date), yend = .data$deaths), linetype = 2) +
-    #   geom_text(aes(x = max(.data$date), label = .data$state), hjust = 0) +
-    #   coord_cartesian(clip = "off")
-    #
-    # file_name <- "anim_plot_uf_mortes"
 
   }
-
-  # if (anim == TRUE) {
-  #
-  #   if (!dir.exists(dir)) {
-  #     dir.create(dir)
-  #   }
-  #
-  #   message(paste0("Salvando anim na pasta ", dir, "/ ..."))
-  #
-  #   gganimate::anim_save(
-  #     filename = paste0(dir, "/", file_name, ".gif"),
-  #     animation = anim_plot
-  #   )
-  #
-  # }
 
   print(state_plot)
 
