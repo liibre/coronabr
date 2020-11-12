f7e8728fe31256a9b0d3de3368391119c53912f2
Andrea Sánchez Tapia <katori@gmail.com>
2020-11-12 13:54:12 -0800

    help geral

---
+++ R/coronabr.R
@@ -0,0 +1,9 @@
+#' coronabr: Download de dados de COVID-19
+#'
+#' coronabr coronabr é um pacote de R para fazer download e visualizar os dados
+#' dos casos diários de coronavírus (COVID-19) disponibilizados por diferentes
+#' fontes
+#'
+#' @docType package
+#' @name coronabr
+NULL
